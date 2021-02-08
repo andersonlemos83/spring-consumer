13ce35ab4017c0248089c10cb2cc64189aef4f79
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2021-02-08 15:57:03 -0300

    Consumidor do curso RabbitMQ com SpringBoot e Docker

---
+++ src/main/java/com/example/spring/consumer/amqp/impl/RabbitMQConsumer.java
@@ -0,0 +1,26 @@
+package com.example.spring.consumer.amqp.impl;
+
+import com.example.spring.consumer.amqp.AmqpConsumer;
+import com.example.spring.consumer.dto.Message;
+import com.example.spring.consumer.service.ConsumerService;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConsumer implements AmqpConsumer<Message> {
+
+    @Autowired
+    private ConsumerService consumerService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.request.routing-key.producer}")
+    @Override
+    public void consumer(Message message) {
+        try {
+            consumerService.action(message);
+        } catch (Exception excecao) {
+            throw new AmqpRejectAndDontRequeueException(excecao);
+        }
+    }
+}
